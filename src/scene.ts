807b3134fc1fc2e155a0bd83fc157ebc39d7623d
koji <kk2796@nyu.edu>
2023-11-09 03:40:11 -0500

    load glb

--- src/scene.ts
+++ src/scene.ts
@@ -19,6 +19,7 @@ import {
 } from 'three'
 import { DragControls } from 'three/examples/jsm/controls/DragControls'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 import * as animations from './helpers/animations'
 import { toggleFullScreen } from './helpers/fullscreen'
@@ -33,15 +34,17 @@ let scene: Scene
 let loadingManager: LoadingManager
 let ambientLight: AmbientLight
 let pointLight: PointLight
-let cube: Mesh
+let sofa: THREE.Group
+let sofaMesh: Mesh
 let camera: PerspectiveCamera
 let cameraControls: OrbitControls
 let dragControls: DragControls
-let axesHelper: AxesHelper
-let pointLightHelper: PointLightHelper
+// let axesHelper: AxesHelper
+// let pointLightHelper: PointLightHelper
 let clock: Clock
 let stats: Stats
 let gui: GUI
+let loader = new GLTFLoader()
 
 const animation = { enabled: false, play: true }
 
@@ -80,7 +83,7 @@ function init() {
 
   // ===== 💡 LIGHTS =====
   {
-    ambientLight = new AmbientLight('white', 0.4)
+    ambientLight = new AmbientLight('white', 1.0)
     pointLight = new PointLight('#ffdca8', 1.2, 100)
     pointLight.position.set(-2, 3, 3)
     pointLight.castShadow = true
@@ -95,16 +98,105 @@ function init() {
 
   // ===== 📦 OBJECTS =====
   {
-    const sideLength = 1
-    const cubeGeometry = new BoxGeometry(sideLength, sideLength, sideLength)
-    const cubeMaterial = new MeshStandardMaterial({
-      color: '#f69f1f',
-      metalness: 0.5,
-      roughness: 0.7,
-    })
-    cube = new Mesh(cubeGeometry, cubeMaterial)
-    cube.castShadow = true
-    cube.position.y = 0.5
+    loader.load(
+      'sofa.glb',
+      (gltf) => {
+        sofa = gltf.scene
+        sofa.castShadow = true
+        sofa.position.y = 0.4
+        sofa.traverse((child) => {
+          if (child instanceof Mesh) {
+            // Apply the texture to the material of the model
+            sofaMesh = child.material
+          }
+        })
+        scene.add(sofa)
+
+        // initialize the camera control
+        cameraControls = new OrbitControls(camera, canvas)
+        cameraControls.target = sofa.position.clone()
+        cameraControls.enableDamping = true
+        cameraControls.autoRotate = false
+        cameraControls.update()
+
+        const cameraFolder = gui.addFolder('Camera')
+        cameraFolder.add(cameraControls, 'autoRotate')
+
+        const sofaOneFolder = gui.addFolder('sofa')
+
+        sofaOneFolder
+          .add(sofa.position, 'x')
+          .min(-5)
+          .max(5)
+          .step(0.5)
+          .name('pos x')
+        sofaOneFolder
+          .add(sofa.position, 'y')
+          .min(-5)
+          .max(5)
+          .step(0.5)
+          .name('pos y')
+        sofaOneFolder
+          .add(sofa.position, 'z')
+          .min(-5)
+          .max(5)
+          .step(0.5)
+          .name('pos z')
+
+        sofaOneFolder.add(sofaMesh, 'wireframe')
+        sofaOneFolder.addColor(sofaMesh, 'color')
+        sofaOneFolder.add(sofaMesh, 'metalness', 0, 1, 0.1)
+        sofaOneFolder.add(sofaMesh, 'roughness', 0, 1, 0.1)
+
+        sofaOneFolder
+          .add(sofa.rotation, 'x', -Math.PI * 2, Math.PI * 2, Math.PI / 4)
+          .name('rotate x')
+        sofaOneFolder
+          .add(sofa.rotation, 'y', -Math.PI * 2, Math.PI * 2, Math.PI / 4)
+          .name('rotate y')
+        sofaOneFolder
+          .add(sofa.rotation, 'z', -Math.PI * 2, Math.PI * 2, Math.PI / 4)
+          .name('rotate z')
+
+        sofaOneFolder.add(animation, 'enabled').name('animated')
+
+        // drag control
+        dragControls = new DragControls([sofa], camera, renderer.domElement)
+        dragControls.addEventListener('hoveron', (event) => {
+          event.object.material.emissive.set('orange')
+        })
+        dragControls.addEventListener('hoveroff', (event) => {
+          event.object.material.emissive.set('black')
+        })
+        dragControls.addEventListener('dragstart', (event) => {
+          cameraControls.enabled = false
+          animation.play = false
+          event.object.material.emissive.set('black')
+          event.object.material.opacity = 0.7
+          event.object.material.needsUpdate = true
+        })
+        dragControls.addEventListener('dragend', (event) => {
+          cameraControls.enabled = true
+          animation.play = true
+          event.object.material.emissive.set('black')
+          event.object.material.opacity = 1
+          event.object.material.needsUpdate = true
+        })
+        dragControls.enabled = false
+
+        const controlsFolder = gui.addFolder('Controls')
+        controlsFolder.add(dragControls, 'enabled').name('drag controls')
+
+        // light
+        const lightsFolder = gui.addFolder('Lights')
+        lightsFolder.add(pointLight, 'visible').name('point light')
+        lightsFolder.add(ambientLight, 'visible').name('ambient light')
+      },
+      undefined,
+      (error) => {
+        console.error(error)
+      }
+    )
 
     const planeGeometry = new PlaneGeometry(3, 3)
     const planeMaterial = new MeshLambertMaterial({
@@ -118,48 +210,21 @@ function init() {
     const plane = new Mesh(planeGeometry, planeMaterial)
     plane.rotateX(Math.PI / 2)
     plane.receiveShadow = true
-
-    scene.add(cube)
-    scene.add(plane)
   }
 
   // ===== 🎥 CAMERA =====
   {
-    camera = new PerspectiveCamera(50, canvas.clientWidth / canvas.clientHeight, 0.1, 100)
+    camera = new PerspectiveCamera(
+      50,
+      canvas.clientWidth / canvas.clientHeight,
+      0.1,
+      100
+    )
     camera.position.set(2, 2, 5)
   }
 
   // ===== 🕹️ CONTROLS =====
   {
-    cameraControls = new OrbitControls(camera, canvas)
-    cameraControls.target = cube.position.clone()
-    cameraControls.enableDamping = true
-    cameraControls.autoRotate = false
-    cameraControls.update()
-
-    dragControls = new DragControls([cube], camera, renderer.domElement)
-    dragControls.addEventListener('hoveron', (event) => {
-      event.object.material.emissive.set('orange')
-    })
-    dragControls.addEventListener('hoveroff', (event) => {
-      event.object.material.emissive.set('black')
-    })
-    dragControls.addEventListener('dragstart', (event) => {
-      cameraControls.enabled = false
-      animation.play = false
-      event.object.material.emissive.set('black')
-      event.object.material.opacity = 0.7
-      event.object.material.needsUpdate = true
-    })
-    dragControls.addEventListener('dragend', (event) => {
-      cameraControls.enabled = true
-      animation.play = true
-      event.object.material.emissive.set('black')
-      event.object.material.opacity = 1
-      event.object.material.needsUpdate = true
-    })
-    dragControls.enabled = false
-
     // Full screen
     window.addEventListener('dblclick', (event) => {
       if (event.target === canvas) {
@@ -170,17 +235,15 @@ function init() {
 
   // ===== 🪄 HELPERS =====
   {
-    axesHelper = new AxesHelper(4)
-    axesHelper.visible = false
-    scene.add(axesHelper)
-
-    pointLightHelper = new PointLightHelper(pointLight, undefined, 'orange')
-    pointLightHelper.visible = false
-    scene.add(pointLightHelper)
-
-    const gridHelper = new GridHelper(20, 20, 'teal', 'darkgray')
-    gridHelper.position.y = -0.01
-    scene.add(gridHelper)
+    // axesHelper = new AxesHelper(4)
+    // axesHelper.visible = false
+    // scene.add(axesHelper)
+    // pointLightHelper = new PointLightHelper(pointLight, undefined, 'orange')
+    // pointLightHelper.visible = false
+    // scene.add(pointLightHelper)
+    // const gridHelper = new GridHelper(20, 20, 'teal', 'darkgray')
+    // gridHelper.position.y = -0.01
+    // scene.add(gridHelper)
   }
 
   // ===== 📈 STATS & CLOCK =====
@@ -194,36 +257,9 @@ function init() {
   {
     gui = new GUI({ title: '🐞 Debug GUI', width: 300 })
 
-    const cubeOneFolder = gui.addFolder('Cube one')
-
-    cubeOneFolder.add(cube.position, 'x').min(-5).max(5).step(0.5).name('pos x')
-    cubeOneFolder.add(cube.position, 'y').min(-5).max(5).step(0.5).name('pos y')
-    cubeOneFolder.add(cube.position, 'z').min(-5).max(5).step(0.5).name('pos z')
-
-    cubeOneFolder.add(cube.material, 'wireframe')
-    cubeOneFolder.addColor(cube.material, 'color')
-    cubeOneFolder.add(cube.material, 'metalness', 0, 1, 0.1)
-    cubeOneFolder.add(cube.material, 'roughness', 0, 1, 0.1)
-
-    cubeOneFolder.add(cube.rotation, 'x', -Math.PI * 2, Math.PI * 2, Math.PI / 4).name('rotate x')
-    cubeOneFolder.add(cube.rotation, 'y', -Math.PI * 2, Math.PI * 2, Math.PI / 4).name('rotate y')
-    cubeOneFolder.add(cube.rotation, 'z', -Math.PI * 2, Math.PI * 2, Math.PI / 4).name('rotate z')
-
-    cubeOneFolder.add(animation, 'enabled').name('animated')
-
-    const controlsFolder = gui.addFolder('Controls')
-    controlsFolder.add(dragControls, 'enabled').name('drag controls')
-
-    const lightsFolder = gui.addFolder('Lights')
-    lightsFolder.add(pointLight, 'visible').name('point light')
-    lightsFolder.add(ambientLight, 'visible').name('ambient light')
-
-    const helpersFolder = gui.addFolder('Helpers')
-    helpersFolder.add(axesHelper, 'visible').name('axes')
-    helpersFolder.add(pointLightHelper, 'visible').name('pointLight')
-
-    const cameraFolder = gui.addFolder('Camera')
-    cameraFolder.add(cameraControls, 'autoRotate')
+    // const helpersFolder = gui.addFolder('Helpers')
+    // helpersFolder.add(axesHelper, 'visible').name('axes')
+    // helpersFolder.add(pointLightHelper, 'visible').name('pointLight')
 
     // persist GUI state in local storage on changes
     gui.onFinishChange(() => {
@@ -252,8 +288,8 @@ function animate() {
   stats.update()
 
   if (animation.enabled && animation.play) {
-    animations.rotate(cube, clock, Math.PI / 3)
-    animations.bounce(cube, clock, 1, 0.5, 0.5)
+    animations.rotate(sofa, clock, Math.PI / 3)
+    animations.bounce(sofa, clock, 1, 0.5, 0.5)
   }
 
   if (resizeRendererToDisplaySize(renderer)) {
@@ -262,7 +298,9 @@ function animate() {
     camera.updateProjectionMatrix()
   }
 
-  cameraControls.update()
+  if (cameraControls) {
+    cameraControls.update()
+  }
 
   renderer.render(scene, camera)
 }
